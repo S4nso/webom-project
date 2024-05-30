f3f3bc1713b48ca211e577af9ae2b8f817c938c9
Dxhyeon <kimdxhyeon@gmail.com>
2024-05-30 15:48:05 +0900

    Initial commit

---
+++ webom/src/test/java/webom/webom/WebomApplicationTests.java
@@ -0,0 +1,13 @@
+package webom.webom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
